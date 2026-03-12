98a6cf12840595c67f1f2742482ab50849a8828e
DuongNguyen001 <duongusa2004nguyen@gmail.com>
2026-03-12 08:17:38 -0400

    fix magic detection for render

--- features/magic.py
+++ features/magic.py
@@ -1,10 +1,12 @@
-import magic
+def get_file_type(file_path):
+    with open(file_path, "rb") as f:
+        header = f.read(4)
 
-def get_file_type(path):
-
-    try:
-        file_type = magic.from_file(path, mime=True)
-        return file_type
-
-    except:
+    if header.startswith(b'\xFF\xD8'):
+        return "jpg"
+    elif header.startswith(b'\x89PNG'):
+        return "png"
+    elif header.startswith(b'%PDF'):
+        return "pdf"
+    else:
         return "unknown"
